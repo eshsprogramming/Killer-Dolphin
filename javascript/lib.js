32974737a97cdb0c577a8d83cb1e0fe5f6c4d4b5
Benjamin Landers <benjaminrlanders@gmail.com>
2013-11-01 14:18:55 -0700

    added bottle array

--- javascript/lib.js
+++ javascript/lib.js
@@ -1,1 +1,35 @@
 //Library
+var bottles = [];
+function Bottle(x,y,dx,dy){
+	this.sprite = new Kinetic.Sprite({
+    	x: x,
+    	y:y,
+    	image: bottlesheet,
+    	animation: 'fly',
+    	animations: bottleanim,
+    	frameRate: 12,
+    	width: 256,
+    	height: 256,
+    	scaleX: .25,
+    	scaleY: .25
+	});
+	this.dy = dy;
+	this.dx = dx;
+	this.step = function(t){
+		this.sprite.setX(this.sprite.getX()+t*dx);
+		this.sprite.setY(this.sprite.getY()+t*dy);
+	};
+}
+var number = 1;
+function start(){
+	number--
+	if(number <= 0){
+		stage.add(ground);
+  		stage.add(playerLayer);
+  		for(var i = 0; i < bottles.length;i++)
+  			hud.add(bottles[i].sprite);
+  		stage.add(hud);
+  		for(var i = 0; i < bottles.length;i++)
+ 			bottles[i].sprite.start()
+	}
+}
--- javascript/main.js
+++ javascript/main.js
@@ -10,24 +10,8 @@ var hud = new Kinetic.Layer();
 
 var bottlesheet = new Image();
 bottlesheet.onload = function(){
-     bottle = new Kinetic.Sprite({
-    	x: 0,
-    	y:0,
-    	image: bottlesheet,
-    	animation: 'fly',
-    	animations: bottleanim,
-    	frameRate: 12,
-    	width: 256,
-    	height: 256,
-    	scaleX: .25,
-    	scaleY: .25
-  });
-  stage.add(ground);
-  stage.add(playerLayer);
-  hud.add(bottle);
-
-  stage.add(hud);
-  bottle.start();
-  
+    bottles[0] = new Bottle(0,0,1,1);
+    bottles[1] = new Bottle(0,64,1,1);
+    start();
 }
 bottlesheet.src = "res/bottle.png"

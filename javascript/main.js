992a3dca10f275f8e989964772722060cca964e1
Benjamin Landers <benjaminrlanders@gmail.com>
2013-11-01 11:54:37 -0700

    added abstraction

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,9 @@
-var yolo = "hello";
+var stage = new Kinetic.Stage({
+  container: 'container',
+  width: 1000,
+  height: 400
+});
+
+var playerLayer = new Kinetic.Layer();
+var ground = new Kinetic.Layer();
+var hud = new Kinetic.Layer();

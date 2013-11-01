7a0ffd014faad457dabb034829f77125cccf7043
Benjamin Landers <benjaminrlanders@gmail.com>
2013-11-01 11:32:02 -0700

    added animation code

---
+++ javascript/animation.js
@@ -0,0 +1,18 @@
+function AnimationSet(data){
+  var out = {};
+  for(var i = 0; i < data.anims.length; i++){
+    out[data.anims[i].name]=Animation(data.anims[i].slides,data);
+  }
+  return out;
+}
+function Animation(slides, data){
+  var x,y;
+  var out=[];
+  for(var i = 0; i < slides.length; i++){
+    x = slides[i]%data.columns;
+    y = Math.floor(slides[i]/data.columns);
+    out[i] = {'x': x*data.width, 'y' : y*data.height,
+           'width': data.width, 'height': data.height}
+  }
+  return out;
+}
